1e7a9e0fd1d8d84e6c83a8b74f2c7c72c1f18b66
Neil Swing <njeswing23@bpsu.edu.ph>
2023-10-10 20:41:20 +0800

    up

--- client/src/components/Input/Footer.tsx
+++ client/src/components/Input/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
     <div className="hidden px-3 pb-1 pt-2 text-center text-xs text-black/50 dark:text-white/50 md:block md:px-4 md:pb-4 md:pt-3">
       <a href="https://bingblong.com" target="_blank" rel="noreferrer" className="underline">
         {config?.appTitle || 'BingBlong'} -To use this site click the icon and choose the blue icon
-        named ChatGPT
+        named ChatGPT!
       </a>
     </div>
   );
